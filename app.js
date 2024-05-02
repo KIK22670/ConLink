f98aa0845d13c52c858bb58db0a8585e470b3a60
KIK22670 <kikicaleksandra@gmail.com>
2024-05-03 00:15:25 +0200

    Passwort zurücksetzen vollständig

--- app.js
+++ app.js
@@ -130,8 +130,104 @@ const emailVerificationTemplate = (verificationToken) => `
 
 `;
 
+const resetPasswordTemplate = (token) => `
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Passwort zurücksetzen!</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+        }
+
+        .card {
+            border-radius: 10px;
+            overflow: hidden;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        }
+
+        .card-header {
+            background-color: #1b9aaa;
+            color: #fff;
+            padding: 20px;
+            border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+            text-align: center;
+        }
+
+        .card-body {
+            padding: 20px;
+            text-align: center;
+        }
+
+        h1 {
+            margin-top: 0;
+            font-size: 24px;
+        }
+
+        p {
+            margin-bottom: 20px;
+            color: #555;
+            line-height: 1.6;
+        }
+
+        .verification-link a {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #1b9aaa;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+            transition: background-color 0.3s ease;
+        }
+
+        .verification-link a:hover {
+            background-color: #0f7c8a;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h1>E-Mail-Verifizierung Erforderlich</h1>
+            </div>
+            <div class="card-body">
+                <p>Bitte klicken Sie auf den folgenden Link, um Ihr Passwort zurückzusetzen</p>
+                <div class="verification-link">
+                    <a href="http://localhost:3001/reset-password/${token}">Verify Email</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+
+
+`;
+
 app.get('/email-verification', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'emailverification.html'));
+});
+
+app.get('/resetpasswordverification', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'resetpasswordverification.html'));
 });
 
 app.get('/registration', (req, res) => {
@@ -293,61 +389,6 @@ app.post('/login', async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 });
-
-
-/* app.post('/login', async (req, res) => {
-    try {
-        const { email, passwort } = req.body;
-
-        // Log Request Body
-        console.log('Request Body:', req.body);
-
-        // Check if password is provided
-        if (!passwort) {
-            console.log('Password is required');
-            return res.status(400).json({ error: 'Password is required' });
-        }
-
-        // Database Query
-        const query = {
-            text: 'SELECT * FROM u_userverwaltung WHERE LOWER(u_email) = LOWER($1)',
-            values: [email.toLowerCase()],
-        };
-
-        const result = await client.query(query);
-
-        // Log Database Query Result
-        console.log('Database Query Result:', result.rows);
-
-        if (result.rows.length === 1) {
-            console.log('User found in the database');
-            const user = result.rows[0];
-
-            if (user.u_passwort) {
-                console.log('User has a hashed password');
-
-                // Check if hashed password is defined
-                if (bcrypt.compareSync(passwort, user.u_passwort)) {
-                    console.log('Password comparison successful');
-                    req.session.user = { id: user.u_id, email: user.u_email };
-                    res.redirect('/doctorsearch');
-                } else {
-                    console.log('Incorrect email or password');
-                    res.status(401).json({ error: 'Invalid email or password' });
-                }
-            } else {
-                console.log('User does not have a hashed password');
-                res.status(401).json({ error: 'Invalid email or password' });
-            }
-        } else {
-            console.log('No user found with the provided email');
-            res.status(401).json({ error: 'Invalid email or password' });
-        }
-    } catch (error) {
-        console.error('Error during login:', error);
-        res.status(500).json({ error: error.message });
-    }
-}); */
 
 
 app.get('/logout', (req, res) => {
@@ -524,26 +565,6 @@ app.post('/speichereTermin', async (req, res) => {
     }
 });
 
-/* app.post('/aktualisiereNotiz', async (req, res) => {
-  try {
-    // Get appointment ID and new note from request body
-    const appointmentID = req.body.appointmentID;
-    const newNote = req.body.newNote;
-
-    // Update the note in the database
-    const query = {
-      text: 'UPDATE t_termine SET t_notizen = $1 WHERE t_id = $2',
-      values: [newNote, appointmentID],
-    };
-    await client.query(query);
-
-    // Send success response
-    res.status(200).json({ success: true });
-  } catch (error) {
-    console.error('Error updating note:', error);
-    res.status(500).json({ success: false, error: 'Internal Server Error' });
-  }
-}); */
 
 
 app.post('/storniereTermin', async (req, res) => {
@@ -600,7 +621,123 @@ app.get('/holetermine', async (req, res) => {
     }
 });
 
-app.listen(port, () => {
-    console.log(`Server läuft auf http://localhost:${port}`);
+// Forgot Password validation
+function validateResetInput(data) {
+    let errors = {};
+
+    data.email = data.email.trim();
+
+    if (!data.email) {
+        errors.email = 'Email is required';
+    } else if (!/^\S+@\S+\.\S+$/.test(data.email)) {
+        errors.email = 'Email is invalid';
+    }
+
+    return {
+        errors,
+        isValid: Object.keys(errors).length === 0,
+    };
+}
+
+// Forgot Password Route
+app.post('/forgot-password', async (req, res) => {
+    const { errors, isValid } = validateResetInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const token = crypto.randomBytes(48).toString('hex');
+    const expirationTime = new Date(Date.now() + 3600000); // 1 hour expiration
+
+    try {
+        // Check if user with provided email exists
+        const userResult = await client.query('SELECT * FROM u_userverwaltung WHERE u_email = $1', [req.body.email]);
+        if (userResult.rows.length === 0) {
+            return res.status(400).json({ email: 'Invalid email address' });
+        }
+
+        // Save token and expiration date in database
+        await client.query(
+            'UPDATE u_userverwaltung SET resetpasswordtoken = $1, resetpasswordexpires = $2 WHERE u_email = $3',
+            [token, expirationTime, req.body.email]
+        );
+
+        // Send email to user
+        const mailOptions = {
+            to: req.body.email,
+            from: 'kikicaleksandra@gmail.com',
+            subject: 'Password Reset',
+            html: resetPasswordTemplate(token)
+        };
+        await sgMail.send(mailOptions);
+
+        res.status(200).sendFile(path.join(__dirname, 'public', 'resetpasswordverification.html'));
+    } catch (error) {
+        console.error('Error resetting password:', error);
+        res.status(500).json("Internal Server Error");
+    }
 });
 
+// Reset Password Route
+app.post('/reset-password/:token', async (req, res) => {
+    const { token } = req.params;
+    const { password, confirmPassword } = req.body;
+
+    if (password !== confirmPassword) {
+        return res.status(400).json({ confirmPassword: 'Passwords do not match' });
+    }
+
+    try {
+        // Check if the token is valid and not expired
+        const userResult = await client.query(
+            'SELECT * FROM u_userverwaltung WHERE resetpasswordtoken = $1 AND resetpasswordexpires > CURRENT_TIMESTAMP',
+            [token]
+        );
+
+        if (userResult.rows.length === 0) {
+            return res.status(400).json({ token: 'Token is invalid or has expired' });
+        }
+
+        // Update user password
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await client.query(
+            'UPDATE u_userverwaltung SET u_passwort = $1, resetpasswordtoken = NULL, resetpasswordexpires = NULL WHERE resetpasswordtoken = $2',
+            [hashedPassword, token]
+        );
+
+        res.status(200).sendFile(path.join(__dirname, 'public', 'resetsuccess.html'));
+    } catch (error) {
+        console.error('Error resetting password:', error);
+        res.status(500).json("Internal Server Error");
+    }
+});
+
+// Reset Password Route
+app.get('/reset-password/:token', async (req, res) => {
+    const { token } = req.params;
+
+    try {
+        // Check if the token is valid and not expired
+        const userResult = await client.query(
+            'SELECT * FROM u_userverwaltung WHERE resetpasswordtoken = $1 AND resetpasswordexpires > CURRENT_TIMESTAMP',
+            [token]
+        );
+
+        if (userResult.rows.length === 0) {
+            return res.status(400).json({ token: 'Token is invalid or has expired' });
+        }
+
+        // If the token is valid, render the HTML page for resetting the password
+        res.sendFile(path.join(__dirname, 'public', 'newpassword.html'));
+    } catch (error) {
+        console.error('Error resetting password:', error);
+        res.status(500).json("Internal Server Error");
+    }
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
